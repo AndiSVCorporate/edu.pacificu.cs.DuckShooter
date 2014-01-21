e9aa9973e4c593f8dd53cad3dfc9311eafabdb66
schl4159 <brandonschlack@pacificu.edu>
2014-01-21 15:24:10 -0800

    Working Fullscreen/Immersive Mode for Main and Game Activity

--- ShooterSwag/src/edu/pacificu/cs/duckhunter/DuckHunterMain.java
+++ ShooterSwag/src/edu/pacificu/cs/duckhunter/DuckHunterMain.java
@@ -1,49 +1,17 @@
 package edu.pacificu.cs.duckhunter;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.WindowManager.LayoutParams;
 import android.widget.Button;
-import edu.pacificu.cs.duckhunter.util.SystemUiHider;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- * 
- * @see SystemUiHider
- */
 public class DuckHunterMain extends Activity
 {
-  /**
-   * Whether or not the system UI should be auto-hidden after
-   * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-   */
-  private static final boolean AUTO_HIDE = true;
-
-  /**
-   * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after user
-   * interaction before hiding the system UI.
-   */
-  private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-  /**
-   * If set, will toggle the system UI visibility upon interaction. Otherwise,
-   * will show the system UI visibility upon interaction.
-   */
-  private static final boolean TOGGLE_ON_CLICK = true;
-
-  /**
-   * The flags to pass to {@link SystemUiHider#getInstance}.
-   */
-  private static final int HIDER_FLAGS = SystemUiHider.FLAG_FULLSCREEN;
-
-  /**
-   * The instance of the {@link SystemUiHider} for this activity.
-   */
-  private SystemUiHider mSystemUiHider;
 
   @Override
   protected void onCreate (Bundle savedInstanceState)
@@ -51,8 +19,8 @@ public class DuckHunterMain extends Activity
     super.onCreate (savedInstanceState);
 
     setContentView (R.layout.activity_duck_hunter_main);
-
-    final View contentView = findViewById (R.id.fullscreen_content);
+    toggleUI ();
+    
     final Button btnNewGame = (Button) findViewById (R.id.btnNewGame);
     btnNewGame.setOnClickListener (new View.OnClickListener()
     {
@@ -63,130 +31,33 @@ public class DuckHunterMain extends Activity
         startGame();       
       }
     });
-
-    // Set up an instance of SystemUiHider to control the system UI for
-    // this activity.
-    mSystemUiHider = SystemUiHider.getInstance (this, contentView, HIDER_FLAGS);
-    mSystemUiHider.setup ();
-    /*mSystemUiHider
-        .setOnVisibilityChangeListener (new SystemUiHider.OnVisibilityChangeListener ()
-        {
-          // Cached values.
-          int mControlsHeight;
-          int mShortAnimTime;
-
-          @Override
-          @TargetApi (Build.VERSION_CODES.HONEYCOMB_MR2)
-          public void onVisibilityChange (boolean visible)
-          {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2)
-            {
-              // If the ViewPropertyAnimator API is available
-              // (Honeycomb MR2 and later), use it to animate the
-              // in-layout UI controls at the bottom of the
-              // screen.
-              if (mControlsHeight == 0)
-              {
-                mControlsHeight = controlsView.getHeight ();
-              }
-              if (mShortAnimTime == 0)
-              {
-                mShortAnimTime = getResources ().getInteger (
-                    android.R.integer.config_shortAnimTime);
-              }
-              controlsView.animate ()
-                  .translationY (visible ? 0 : mControlsHeight)
-                  .setDuration (mShortAnimTime);
-            }
-            else
-            {
-              // If the ViewPropertyAnimator APIs aren't
-              // available, simply show or hide the in-layout UI
-              // controls.
-              controlsView.setVisibility (visible ? View.VISIBLE : View.GONE);
-            }
-
-            if (visible && AUTO_HIDE)
-            {
-              // Schedule a hide().
-              delayedHide (AUTO_HIDE_DELAY_MILLIS);
-            }
-          }
-        });*/
-
-    // Set up the user interaction to manually show or hide the system UI.
-    contentView.setOnClickListener (new View.OnClickListener ()
-    {
-      @Override
-      public void onClick (View view)
-      {
-        if (TOGGLE_ON_CLICK)
-        {
-          mSystemUiHider.toggle ();
-        }
-        else
-        {
-          mSystemUiHider.show ();
-        }
-      }
-    });
-
-    // Upon interacting with UI controls, delay any scheduled hide()
-    // operations to prevent the jarring behavior of controls going away
-    // while interacting with the UI.
-  /*  findViewById (R.id.btnNewGame).setOnTouchListener (
-        mDelayHideTouchListener);*/
   }
- 
-
-  @Override
-  protected void onPostCreate (Bundle savedInstanceState)
+  
+  @SuppressLint ("InlinedApi")
+  public void toggleUI()
   {
-    super.onPostCreate (savedInstanceState);
 
-    // Trigger the initial hide() shortly after the activity has been
-    // created, to briefly hint to the user that UI controls
-    // are available.
-    delayedHide (100);
-  }
-
-  /**
-   * Touch listener to use for in-layout UI controls to delay hiding the system
-   * UI. This is to prevent the jarring behavior of controls going away while
-   * interacting with activity UI.
-   */
-  View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener ()
-  {
-    @Override
-    public boolean onTouch (View view, MotionEvent motionEvent)
+    int uiOptions = getWindow().getDecorView().getSystemUiVisibility();
+    int newUiOptions = uiOptions;
+    
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) 
     {
-      if (AUTO_HIDE)
-      {
-        delayedHide (AUTO_HIDE_DELAY_MILLIS);
-      }
-      return false;
+      newUiOptions ^= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+      newUiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
     }
-  };
-
-  Handler mHideHandler = new Handler ();
-  Runnable mHideRunnable = new Runnable ()
-  {
-    @Override
-    public void run ()
+    else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) 
     {
-      mSystemUiHider.hide ();
+      newUiOptions ^= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+      newUiOptions ^= View.SYSTEM_UI_FLAG_FULLSCREEN;
     }
-  };
+    else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) 
+    {
+      newUiOptions ^= View.SYSTEM_UI_FLAG_LOW_PROFILE;
+    }
 
-  /**
-   * Schedules a call to hide() in [delay] milliseconds, canceling any
-   * previously scheduled calls.
-   */
-  private void delayedHide (int delayMillis)
-  {
-    mHideHandler.removeCallbacks (mHideRunnable);
-    mHideHandler.postDelayed (mHideRunnable, delayMillis);
+    getWindow().getDecorView().setSystemUiVisibility(newUiOptions);
   }
+
   
   private void startGame ()
   {
